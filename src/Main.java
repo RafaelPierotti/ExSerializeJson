66f4cc67ce901dbe46983d563e4b84c9d80ecd84
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-11-21 17:00:10 -0300

    JSON Bonito

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Main {
     public static void main(String[] args) {
         Titulo titulo = new Titulo();
 
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+
         String json = gson.toJson(titulo);
 
         System.out.println(json);
